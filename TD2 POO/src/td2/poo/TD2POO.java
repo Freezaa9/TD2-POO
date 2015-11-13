e6fcc25b0eabc8009e989907577b5b8a2dcec746
Geoffrey-H95 <geoffreyharrazi@gmail.com>
2015-11-13 15:27:49 +0100

    Ajout Triangle

--- TD2 POO/src/td2/poo/TD2POO.java
+++ TD2 POO/src/td2/poo/TD2POO.java
@@ -16,17 +16,17 @@ public class TD2POO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Point p1 = new Point(4, 0);
-        Point p2 = new Point(5, 1);
-        Point p3 = new Point(2, 0);
-        Point p4 = new Point(3, 1);
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(2,0);
+        Point p3 = new Point(1, Math.sqrt(3));
+        Point p4 = new Point(-100, 20);
         Point p5 = new Point(2, 4);
         Point p6 = new Point(3, 5);
         Point p7 = new Point(1, 1);
-        Polygone poly1 = new Polygone((new Point[]{p1, p2, p3, p4, p5, p6}));
+        Polygone poly1 = new Polygone((new Point[]{p1, p2, p3, p4}));
         Polygone poly2 = new Polygone((new Point[]{p3, p6, p5, p2, p4, p1}));
-        System.out.println(poly1.perimetre());
-        System.out.println(poly1);
+        Triangle tri = new Triangle(p1,p2,p3);
+        System.out.println(tri);
 
     }
 
--- TD2 POO/src/td2/poo/Triangle.java
+++ TD2 POO/src/td2/poo/Triangle.java
@@ -5,40 +5,49 @@
  */
 package td2.poo;
 
-
 /**
  *
  * @author Geoffrey
  */
 public class Triangle extends Polygone {
 
-	/**
-	 * 
-	 * @param p1
-	 * @param p2
-	 * @param p3
-	 */
-	public Triangle(Point p1, Point p2, Point p3) {
-		super(new Point[]{p1, p2, p3});
-	}
+    /**
+     *
+     * @param p1
+     * @param p2
+     * @param p3
+     */
+    public Triangle(Point p1, Point p2, Point p3) {
+        super(new Point[]{p1, p2, p3});
+    }
 
-	public double surface() {
-            double a = this.getSommets()[0].distance(this.getSommets()[1]);
-            double b = this.getSommets()[1].distance(this.getSommets()[2]);
-            double c = this.getSommets()[2].distance(this.getSommets()[0]);
-            double p = this.perimetre()/2;
-            double result = p;
-            //result += getSommets()[i].distance(getSommets()[(i+1) % getSommets().length]);
-            for (int i = 0; i < 2; i++) {
-                
-            }
-            
-            
-            return Math.sqrt((p*(p-a)*(p-b)*(p-c)));
-	}
+    public double surface() {
+        double p = this.perimetre() / 2;
+        double result = p;
+        for (int i = 0; i < getSommets().length; i++) {
+            result *= (p - (this.getSommets()[i].distance(this.getSommets()[(i + 1) % getSommets().length]))); //copié collé avec toString ? avoir avec méthode surface simplifié
+        }
+        return Math.sqrt(result);
 
-	public String toString() {
-            
-	}
+    }
+
+    public String toString() {
+        double a = this.getSommets()[0].distance(this.getSommets()[(1) % getSommets().length]);
+        double b = this.getSommets()[0].distance(this.getSommets()[(1) % getSommets().length]);
+        double c = this.getSommets()[0].distance(this.getSommets()[(1) % getSommets().length]);
+        StringBuffer buff = new StringBuffer();
+        buff.append("Triangle ");
+        if (a == b && a == c) {
+            buff.append("equilatèrale ");
+        }
+        else if (a==b || a==c || b==c) {
+            buff.append("isocèle ");
+        }
+        else{
+            buff.append("scalène ");
+        }
+        buff.append(this.texteSommets());
+        return buff.toString();
+    }
 
 }
